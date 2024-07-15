bd3a7e92d21c55efcf440321b7ab667c76dcefad
richnstore <zianfth@gmail.com>
2024-07-15 12:20:43 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -80,7 +80,7 @@ wget https://${akbarvpnnnnnnnnn}/edu.sh && chmod +x edu.sh && ./edu.sh
 # Ohp Server
 wget https://${akbarvpnnnnnnnnnn}/ohp.sh && chmod +x ohp.sh && ./ohp.sh
 # notif install
-wget -q https://raw.githubusercontent.com/richnstore/script/main/notif/richnstore && chmod +x richnstore && ./richnstore
+wget -q https://raw.githubusercontent.com/richnstore/scriptvps/main/notif/richnstore && chmod +x richnstore && ./richnstore
 
 rm -f /root/ssh-vpn.sh
 rm -f /root/sstp.sh
